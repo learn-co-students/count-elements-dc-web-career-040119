6b27ea9e92c2b44bee53d7fc1c2bbad36d333126
Daniel Hallada <hallada.daniel@gmail.com>
2019-04-01 17:00:57 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
+require 'pry'
+
 def count_elements(array)
   # code goes here
+  counted_elements = {}
+  array.each do |word|
+  	counted_elements[word] = array.count(word)
+  end 
+  counted_elements
 end
  

3a3111518f4c9bb91b5115b9a448facac88534cf
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-04-02 09:10:13 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,7 +1,12 @@
+# require 'pry'
+
 def count_elements(array)
   count_hash = Hash.new(0)
   #sets default value for any key to 0, while {} sets nil
   # ^ this is why "count_hash = {}" fails
-  array.each {|item| count_hash[item] += 1 }
+  array.each do |item|
+    count_hash[item] += 1
+    # binding.pry
+  end 
   count_hash
 end

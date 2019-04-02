fbc96803a5a5f487f72a53e3195d7b6efab423c7
Justin Wu <justinhwu95@gmail.com>
2019-04-02 00:25:43 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  counter = 0 
+  array.each do |names|
+    counter = array.count(names)
+    new_hash[names] = counter
+  end
+    new_hash
 end
  

7f9553e7921c472f940b467525924a869e43987e
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-04-01 21:05:46 -0400

    complete

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,7 @@
 def count_elements(array)
-  # code goes here
+  count_hash = Hash.new(0)
+  #sets default value for any key to 0, while {} sets nil
+  # ^ this is why "count_hash = {}" fails
+  array.each {|item| count_hash[item] += 1 }
+  count_hash
 end
- 
